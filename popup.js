9b181156f0930a10894ce29ae51b87770dfb4e22
Roman Danilov <r.danylov@gmail.com>
2019-07-25 23:05:17 +0300

    License Added

--- popup.js
+++ popup.js
@@ -1,3 +1,28 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2019 Roman Danilov
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
 "use strict";let servernames=$('#servername').val();let dbnames=$('#dbname').val();let usernames=$('#username').val();let passwords=$('#password').val();let tokens=$('#token').val();let locations=$('#location').val();let categories=$('#categories').val();let subcategories=$('#subcategories').val();let price=$('#price').val();let sorting=$('#sorting').val();let locationArray=[];let categoriesArray=[];let subcategoriesArray=[];const prices=[{id:1,name:"paid"},{id:2,name:"free"}];const sortByDate=[{id:1,name:"date",sorted:"ascending"},{id:2,name:"-date",sorted:"descending"}];$('#token').change(()=>{checkTokens();})
 function checkTokens(){$.ajax({url:'https://www.eventbriteapi.com/v3/system/countries/?token='+$('#token').val(),complete:function(xhr,statusText){if(xhr.status===200){continueScript();}else{alert('Check your token');$('#location, #categories, #subcategories, #price, #sorting').empty();return;}}});};
 function checkDatabase(){$.post("http://luo.cu.ma/events/checkDatabaseConnection.php",{servername:$('#servername').val(),dbname:$('#dbname').val(),username:$('#username').val(),password:$('#password').val()},function(status,data){if(status==="Connection failed"){alert('Check your Database Settings');return;}else{startParse();}});}
